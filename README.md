41cf98ec3645687df55fc6a9171a2bd381a72c0d
Amirul <Amirul109>
2023-05-26 10:24:27 +0700

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![Screenshot (39)](https://github.com/AddiiiSN/Lab7Web/assets/115928747/3e270ec4-03d4-43f6-bbae-dec199316cde)
---
+++ app/Views/template/footer.php
@@ -0,0 +1,49 @@
+  </section>
+  <section id="wrapper">
+      <section id="main">
+          <div class="row">
+              <div class="box">
+                  <img src="https://res.cloudinary.com/dlmjnzi46/image/upload/v1684064265/8597a75e1e352183517c8ad4590c5fa6_dfupun.jpg"
+                      alt="" width="120" length="120" class="image-circle">
+                  <h3>Heading</h3>
+                  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+                  <button class="buttontxt">View detail</button>
+              </div>
+              <div class="box">
+                  <img src="https://res.cloudinary.com/dlmjnzi46/image/upload/v1684064248/55dfcda6d94fd91895b8de35f65e4d20_g5o6xv.jpg"
+                      alt="" width="120" length="120" class="image-circle">
+                  <h3>Heading</h3>
+                  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+                  <button class="buttontxt">View detail</button>
+              </div>
+              <div class="box">
+                  <img src="https://res.cloudinary.com/dlmjnzi46/image/upload/v1684064236/bba3bf45060efa04e8b65d56615c61c7_mdjyoy.jpg"
+                      alt="" width="120" length="120" class="image-circle">
+                  <h3>Heading</h3>
+                  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+                  <button class="buttontxt">View detail</button>
+              </div>
+          </div>
+      </section>
+      <aside id="sidebar">
+          <div class="widget-box">
+              <h3 class="title">Widget Header</h3>
+              <ul>
+                  <li><a href="#">Widget Link</a></li>
+                  <li><a href="#">Widget Link</a></li>
+              </ul>
+          </div>
+          <div class="widget-box">
+              <h3 class="title">Widget Text</h3>
+              <p>Vestibulum lorem elit, iaculis in nisl volutpat, malesuada tincidunt arcu. Proin in leo fringilla,
+                  vestibulum mi porta, faucibus felis. Integer pharetra est nunc, nec pretium nunc pretium ac.</p>
+          </div>
+      </aside>
+  </section>
+  <footer>
+      <p style="text-align:center">Universitas Pelita Bangsa @2023</p>
+  </footer>
+  </div>
+  </body>
+
+  </html>
